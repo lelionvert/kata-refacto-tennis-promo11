a801a4f628a96350a3107c5ec4928ec4e5279dbd
20Bak <back.to.bak@gmail.com>
2019-11-14 14:19:17 +0100

    Extract method to work on it

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -52,27 +52,32 @@ public class TennisGame1 implements TennisGame {
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score.append("-"); tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
+            toto(score);
         }
         return score.toString();
+    }
+
+    private void toto(StringBuilder score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = m_score1;
+            else { score.append("-"); tempScore = m_score2;}
+            switch(tempScore)
+            {
+                case 0:
+                    score.append("Love");
+                    break;
+                case 1:
+                    score.append("Fifteen");
+                    break;
+                case 2:
+                    score.append("Thirty");
+                    break;
+                case 3:
+                    score.append("Forty");
+                    break;
+            }
+        }
     }
 }
