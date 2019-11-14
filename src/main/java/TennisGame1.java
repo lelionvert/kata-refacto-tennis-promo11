36507fee032666772232ee4b03ec1c5dd62ed8b0
20Bak <back.to.bak@gmail.com>
2019-11-14 15:39:19 +0100

    Refactor advantage methods

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -27,20 +27,21 @@ public class TennisGame1 implements TennisGame {
         if (scoreEqual()) {
             return getScoreWhenEqual();
         }
-        int minusResult = scorePlayer1 - scorePlayer2;
-        if (player1getsAdvantage(minusResult)) return "Advantage player1";
-        if (player2getsAdvantage(minusResult)) return "Advantage player2";
-        if ((scorePlayer1 >= 4 || scorePlayer2 >= 4) && minusResult > 1) return "Win for player1";
-        if ((scorePlayer1 >= 4 || scorePlayer2 >= 4) && minusResult < 1) return "Win for player2";
+        if (player1getsAdvantage()) return "Advantage player1";
+        if (player2getsAdvantage()) return "Advantage player2";
+        if ((scorePlayer1 >= 4 || scorePlayer2 >= 4) && scorePlayer1 - scorePlayer2 > 1) return "Win for player1";
+        if ((scorePlayer1 >= 4 || scorePlayer2 >= 4) && scorePlayer1 - scorePlayer2 < 1) return "Win for player2";
 
         return getScoreRegularPlay();
     }
 
-    private boolean player2getsAdvantage(int minusResult) {
+    private boolean player2getsAdvantage() {
+        int minusResult = scorePlayer1 - scorePlayer2;
         return (scorePlayer1 >= 4 || scorePlayer2 >= 4) && minusResult == -1;
     }
 
-    private boolean player1getsAdvantage(int minusResult) {
+    private boolean player1getsAdvantage() {
+        int minusResult = scorePlayer1 - scorePlayer2;
         return (scorePlayer1 >= 4 || scorePlayer2 >= 4) && minusResult == 1;
     }
 
